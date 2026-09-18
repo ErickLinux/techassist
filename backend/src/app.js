138e7836aa8dc3a9edc64f2fe72c78701dbfab5e
ErickLinux <emejiah5@miumg.edu.gt>
2026-09-18 04:34:21 -0600

    revision de solicitud de repuesto2

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,39 +1,93 @@
 import express from "express";
 import cors from "cors";
+
 import adminUsuarioRoutes
   from "./routes/adminUsuarioRoutes.js";
-import usuarioRoutes from "./routes/usuarioRoutes.js";
-import tiendaRoutes from "./routes/tiendaRoutes.js";
-import servicioRoutes from "./routes/servicioRoutes.js";
+
+import usuarioRoutes
+  from "./routes/usuarioRoutes.js";
+
+import tiendaRoutes
+  from "./routes/tiendaRoutes.js";
+
+import servicioRoutes
+  from "./routes/servicioRoutes.js";
+
 import liquidacionRoutes
   from "./routes/liquidacionRoutes.js";
-  import repuestoRoutes
+
+import repuestoRoutes
   from "./routes/repuestoRoutes.js";
-  
+
+
 const app = express();
-app.use(
-  "/api/repuestos",
-  repuestoRoutes
-);
+
+
+// ========================================
+// MIDDLEWARES
+// ========================================
+
 app.use(cors());
 app.use(express.json());
 
+
+// ========================================
+// RUTA PRINCIPAL
+// ========================================
+
 app.get("/", (req, res) => {
+
   res.json({
-    mensaje: "TechAssist API funcionando correctamente"
+    mensaje:
+      "TechAssist API funcionando correctamente"
   });
+
 });
+
+
+// ========================================
+// RUTAS API
+// ========================================
+
 app.use(
   "/api/liquidaciones",
   liquidacionRoutes
 );
 
-app.use("/api/usuarios", usuarioRoutes);
-app.use("/api/tiendas", tiendaRoutes);
-app.use("/api/servicios", servicioRoutes);
+app.use(
+  "/api/usuarios",
+  usuarioRoutes
+);
+
+app.use(
+  "/api/tiendas",
+  tiendaRoutes
+);
+
+app.use(
+  "/api/servicios",
+  servicioRoutes
+);
+
+app.use(
+  "/api/repuestos",
+  repuestoRoutes
+);
+
 app.use(
   "/api/admin/usuarios",
   adminUsuarioRoutes
 );
 
+app.get(
+  "/api/prueba-repuestos",
+  (req, res) => {
+
+    res.json({
+      mensaje:
+        "Modulo de repuestos cargado correctamente"
+    });
+
+  }
+);
 export default app;
