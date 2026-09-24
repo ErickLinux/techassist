4dc69ca66232e885b643dc172da027e1f846a1f8
ErickLinux <emejiah5@miumg.edu.gt>
2026-09-24 06:42:53 -0600

    revision de login2

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -6,17 +6,20 @@ import nodemailer from "nodemailer";
 // ========================================
 
 const transporter = nodemailer.createTransport({
-
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
 
   auth: {
-
     user: process.env.EMAIL_USER,
-
     pass: process.env.EMAIL_APP_PASSWORD
+  },
 
-  }
+  tls: {
+    servername: "smtp.gmail.com"
+  },
 
+  family: 4
 });
 
 
